ed595866e3473c3ecd75a643a4d918d90e2b3a7a
ELY D J SANTOS <132177303+elydaisy@users.noreply.github.com>
2025-09-20 22:33:17 -0300

    att

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     links.forEach(link => {
         link.addEventListener("click", function () {
-            alert("Você será redirecionado para " + this.href);
-        });
+           e.preventDefault(); // impede que redirecione imediatamente
+        const spinner = document.getElementById('loading-spinner');
+        spinner.style.display = 'block'; // mostra o spinner
+
+        setTimeout(() => {
+            window.location.href = this.href; // redireciona depois de um tempo
+        }, 1000); // espera 1 segundo (ou o tempo que quiser)
     });
 });
 
+
+window.addEventListener('load', function() {
+        // Adiciona uma classe que ativa a animação quando a página está carregada
+        const spinner = document.getElementById('loading-spinner');
+
+        //remover o spinner do DOM
+        setTimeout(() => {
+            spinner.remove();
+        }, 1000); // Espera 1 segundo para a animação terminar antes de remover o spinner
+    });
+});
